70ed8695a2555af087b524f6e0d3b96a89abb5be
thomas <thomas-strehler@gmx.de>
2023-08-08 20:51:46 +0200

    dev - databse in function

--- bogan/database/update_database.py
+++ bogan/database/update_database.py
@@ -7,59 +7,64 @@ import os
 
 db_path = os.path.join("data", "spiel2.db")
 
-# def add_partie(spieldaten: list, session: Session) -> Session:
-#     for play in spieldaten:
-#         partie = Partie(id=play['@id'], datum=play['@date'], ort=play['@location'], name=play['item']['@name'])
-#         session.add(partie)
-
-#     return session
-
-# def add_player(player: list, session: Session) -> Session:
-
-
 engine = create_engine(f"sqlite:///{db_path}")
-# Base = declarative_base()
 
 
-# Leere Datenbank
-Base.metadata.drop_all(engine)
-Base.metadata.create_all(engine)
+def check_str2float(str2float: str) -> float:
+    if str2float.isdigit():
+        return float(str2float)
+    else:
+        return None
 
-# Öffne Session
-# Session = sessionmaker(bind=engine)
-session = Session(engine)
+def create_database(clear_all=True):
+    
+    # clear database
+    if clear_all:
+        Base.metadata.drop_all(engine)
+    
+    # Erstelle Datenbank
+    Base.metadata.create_all(engine)
 
-# Leses Daten aus der JSON-Datei
-json_path = os.path.join("data", "plays.json")
-with open(json_path, "r", encoding="utf-16") as f:
-    spieldaten = json.load(f)
 
-# Füge jede Partie zur Datenbank hinzu
-for play in spieldaten["plays"]["play"]:
-    partie = Partie(id=play["@id"], datum=play["@date"], ort=play["@location"], name=play["item"]["@name"])
-    session.add(partie)
+def add_data_to_database():
+    # Öffne Session
+    session = Session(engine)
 
-    # Füge jeden Spieler zur Partie hinzu
-    for player in play["players"]["player"]:
-        # Suche nach dem Spieler in der Benutzer-Tabelle, da Name "unique" kann hier nur 0 oder 1 Ergebnis herauskommen
-        benutzer = session.query(Benutzer).filter_by(name=player["@name"]).first()
-        if benutzer is None:
-            benutzer = Benutzer(name=player["@name"])
-            session.add(benutzer)
+    # Leses Daten aus der JSON-Datei
+    json_path = os.path.join("data", "plays.json")
+    with open(json_path, "r", encoding="utf-16") as f:
+        spieldaten = json.load(f)
 
-        # Erstelle den Spieler und verknüpfe ihn mit dem Benutzer und der Partie
-        # Überprüfe ob Punktzahl vorhanden
-        punktzahl: str = player["@score"]
-        if punktzahl.isdigit():
-            punktzahl = float(punktzahl)
-        else:
-            punktzahl = None
+    # Füge jede Partie zur Datenbank hinzu
+    for play in spieldaten["plays"]["play"]:
+        partie = Partie(id=play["@id"], datum=play["@date"], ort=play["@location"], name=play["item"]["@name"])
+        session.add(partie)
 
-        spieler = Spieler(name=player["@name"], punktzahl=punktzahl, partie=partie, benutzer=benutzer)
-        session.add(spieler)
+        # Füge jeden Spieler zur Partie hinzu
+        for player in play["players"]["player"]:
+            # Suche nach dem Spieler in der Benutzer-Tabelle
+            # Name ist "unique" -> nur 0 oder 1 möglich
+            benutzer = session.query(Benutzer).filter_by(name=player["@name"]).first()
+            if benutzer is None:
+                benutzer = Benutzer(name=player["@name"])
+                session.add(benutzer)
 
-    # Speicher die Änderungen
-    session.commit()
+            # Erstelle den Spieler und verknüpfe ihn mit dem Benutzer und der Partie
+            # Überprüfe ob Punktzahl vorhanden
+            punktzahl = check_str2float(player["@score"])
 
-    # Schließe die Session
-    session.close()
+            spieler = Spieler(name=player["@name"], punktzahl=punktzahl, partie=partie, benutzer=benutzer)
+            session.add(spieler)
+
+        # Speicher die Änderungen
+        session.commit()
+        # Schließe die Session
+        session.close()
+
+def main():
+    create_database()
+    add_data_to_database()
+
+
+if __name__ == "__main__":
+    main()
