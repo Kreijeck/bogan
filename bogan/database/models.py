7ee1203e00789efa8b0416ba3b55d24f1cc0f76b
thomas <thomas-strehler@gmx.de>
2023-08-09 00:11:05 +0200

    Create Database

--- bogan/database/models.py
+++ bogan/database/models.py
@@ -1,5 +1,6 @@
 from typing import List
-from sqlalchemy import String, Float, ForeignKey
+from datetime import date
+from sqlalchemy import String, Float, Integer, Date, ForeignKey
 from sqlalchemy.orm import relationship, declarative_base, Mapped, mapped_column
 
 
@@ -11,18 +12,23 @@ class Partie(Base):
 
     Args:
         id (int): ID aus BGG
-        name (string): Name des Brettspiels
+        brettspiel (string): Link zum Brettspiel
         datum (string): Datum der Partie
-        ort (string): Ort der Partie
+        ort (string): Link zum Ort
         spieler (list): Liste der Spieler, sowie der erzielten Punkte
     """
 
     __tablename__ = "partie"
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String)
-    datum: Mapped[str] = mapped_column(String)
-    ort: Mapped[str] = mapped_column(String)
+    brettspiel_id: Mapped[int] = mapped_column(ForeignKey("brettspiel.id"))
+    brettspiel: Mapped["Brettspiel"] = relationship(back_populates="partie")
+    datum: Mapped[date] = mapped_column(Date)
+    ort_id: Mapped[int] = mapped_column(ForeignKey("ort.id"))
+    ort: Mapped["Ort"] = relationship(back_populates="partie")
     spieler: Mapped[List["Spieler"]] = relationship(back_populates="partie", cascade="all, delete-orphan")
+
+    def __repr__(self) -> str:
+        return f"ID: {self.id}, brettspiel: {self.brettspiel.name}, datum: {self.datum}, ort: {self.ort.name}"
 
 
 class Spieler(Base):
@@ -32,6 +38,8 @@ class Spieler(Base):
         id (int): ID
         name (string): Spielername
         punktzahl (float): erreichte Punkte im Spiel
+        partie_id (int): id der zugehörigen Partie
+        benutzer_id (int): id des benutzers
 
     """
 
@@ -44,16 +52,64 @@ class Spieler(Base):
     benutzer_id: Mapped[int] = mapped_column(ForeignKey("benutzer.id"))
     benutzer: Mapped["Benutzer"] = relationship(back_populates="spieler")
 
+    def __repr__(self) -> str:
+        return f"ID: {self.id}, name: {self.name}, punktzahlt: {self.punktzahl},partie_id: {self.partie_id}, benutzer: {self.benutzer}"
+
 
 class Benutzer(Base):
+    """Alles wissenswerte über einen User
+
+    Args:
+        id (int): id des User
+        name: (str, unique): Name des User
+    """
+
     __tablename__ = "benutzer"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String, unique=True)
     spieler: Mapped[List["Spieler"]] = relationship("Spieler", back_populates="benutzer")
 
+    def __repr__(self) -> str:
+        return f"ID: {self.id}, name:{self.name}"
+
 
 class Brettspiel(Base):
+    """Details zum Brettspiel
+
+    Args:
+        id (int): id des Brettspiels aus BGG
+        name (str): Name des Spiels
+        complexity (float): Komplexität des Spiels aus BGG
+        duration (int): durchschnittliche Dauer eines Spiels
+    """
+
     __tablename__ = "brettspiel"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String)
+    complexity: Mapped[float] = mapped_column(Float, nullable=True)
+    duration: Mapped[int] = mapped_column(Integer, nullable=True)
+    partie: Mapped[List["Partie"]] = relationship(back_populates="brettspiel")
+
+    def __repr__(self) -> str:
+        return f"ID: {self.id}, name: {self.name}, "\
+            f"complexity: {self.complexity or 'na'}, duration: {self.duration or 'na'}"
+
+
+class Ort(Base):
+    """Ort an dem Spiel durchgeführt wurde
+
+    Args:
+        id (int): id des Ortes
+        name (str): Name des Ortes
+    """
+
+    __tablename__ = "ort"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True)
+    partie: Mapped[List["Partie"]] = relationship("Partie", back_populates="ort")
+    # partie: Mapped["Partie"] = relationship(back_populates="ort")
+
+    def __repr__(self) -> str:
+        return f"ID: {self.id}, Name: {self.name}"
--- bogan/database/update_database.py
+++ bogan/database/update_database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from bogan.database.models import Benutzer, Spieler, Partie, Base
+from bogan.database.models import Benutzer, Spieler, Partie, Ort, Brettspiel, Base
+from datetime import datetime
 import json
 import os
 
@@ -16,14 +17,43 @@ def check_str2float(str2float: str) -> float:
     else:
         return None
 
+
 def create_database(clear_all=True):
-    
     # clear database
     if clear_all:
         Base.metadata.drop_all(engine)
-    
+
     # Erstelle Datenbank
     Base.metadata.create_all(engine)
+
+
+def create_ort(play: dict, session: Session) -> Ort:
+    ort = session.query(Ort).filter_by(name=play["@location"]).first()
+    if ort is None:
+        ort = Ort(name=play["@location"])
+        session.add(ort)
+
+    return ort
+
+
+def create_brettspiel(play: dict, session: Session) -> Brettspiel:
+    brettspiel = session.query(Brettspiel).filter_by(name=play["item"]["@name"]).first()
+    if brettspiel is None:
+        brettspiel = Brettspiel(name=play["item"]["@name"])
+        session.add(brettspiel)
+
+    return brettspiel
+
+
+def create_benutzer(player: dict, session: Session) -> Benutzer:
+    # Suche nach dem Spieler in der Benutzer-Tabelle
+    # Name ist "unique" -> nur 0 oder 1 möglich
+    benutzer = session.query(Benutzer).filter_by(name=player["@name"]).first()
+    if benutzer is None:
+        benutzer = Benutzer(name=player["@name"])
+        session.add(benutzer)
+
+    return benutzer
 
 
 def add_data_to_database():
@@ -37,29 +67,33 @@ def add_data_to_database():
 
     # Füge jede Partie zur Datenbank hinzu
     for play in spieldaten["plays"]["play"]:
-        partie = Partie(id=play["@id"], datum=play["@date"], ort=play["@location"], name=play["item"]["@name"])
-        session.add(partie)
+        try:
+            ort = create_ort(play, session)
+            brettspiel = create_brettspiel(play, session)
+            datum = datetime.strptime(play["@date"], "%Y-%m-%d").date()
+            partie = Partie(id=play["@id"], datum=datum, ort=ort, brettspiel=brettspiel)
+            session.add(partie)
 
-        # Füge jeden Spieler zur Partie hinzu
-        for player in play["players"]["player"]:
-            # Suche nach dem Spieler in der Benutzer-Tabelle
-            # Name ist "unique" -> nur 0 oder 1 möglich
-            benutzer = session.query(Benutzer).filter_by(name=player["@name"]).first()
-            if benutzer is None:
-                benutzer = Benutzer(name=player["@name"])
-                session.add(benutzer)
+            # Füge jeden Spieler zur Partie hinzu
+            for player in play["players"]["player"]:
+                benutzer = create_benutzer(player, session)
+                punktzahl = check_str2float(player["@score"])
 
-            # Erstelle den Spieler und verknüpfe ihn mit dem Benutzer und der Partie
-            # Überprüfe ob Punktzahl vorhanden
-            punktzahl = check_str2float(player["@score"])
+                # Erstelle den Spieler und verknüpfe ihn mit dem Benutzer und der Partie
+                # Überprüfe ob Punktzahl vorhanden
+                spieler = Spieler(name=player["@name"], punktzahl=punktzahl, partie=partie, benutzer=benutzer)
+                session.add(spieler)
+        except Exception as e:
+            print(
+                f"Folgendes Spiel hat einen Fehler: Error: {e},"\
+                "Partie_ID: {play['@id']}, Spiel: {play['item']['@name']}"
+            )
 
-            spieler = Spieler(name=player["@name"], punktzahl=punktzahl, partie=partie, benutzer=benutzer)
-            session.add(spieler)
+    # Speicher die Änderungen
+    session.commit()
+    # Schließe die Session
+    session.close()
 
-        # Speicher die Änderungen
-        session.commit()
-        # Schließe die Session
-        session.close()
 
 def main():
     create_database()
