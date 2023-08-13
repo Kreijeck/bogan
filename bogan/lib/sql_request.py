5ea67fdd1454acac8dc9298be0cf674a2efb3ce7
thomas <thomas-strehler@gmx.de>
2023-08-13 22:03:21 +0200

    update .gitignore

---
+++ bogan/lib/sql_request.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from bogan.config import get_logger, get_play_engine
+from bogan.db.models import Benutzer, Partie, SpielerPos, Brettspiel, Ort
+
+log = get_logger(__file__)
+engine = get_play_engine()
+session = Session(engine)
+
+def get_users():
+    ignored_users = ["Bot", "Jörn"]
+    benutzer = select(Benutzer).order_by(Benutzer.id)
+    for ignored_user in ignored_users:
+        benutzer = session.query(benutzer).filter_by(Benutzer.name != ignored_user).all()
+        a  =5 
+        b=6
+        
+
+    return benutzer#session.scalars(stmt)
+
+def get_partien(user):
+    benutzer_id = session.scalars(select(Benutzer.id).where(Benutzer.name==user)).first()
+    stmt = select(Partie).join(Partie.spieler).where(SpielerPos.benutzer_id == benutzer_id)
+    for i, partie in enumerate(session.scalars(stmt)):
+        log.debug(f"Partie {i+1} für {user}: {partie}")
+    return session.scalars(stmt)
+
+
+if __name__ == "__main__":
+    get_partien("Michi")
+    
