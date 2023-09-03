e798baf866f04de42d01176bc2e0037da894ffdd
thomas <thomas-strehler@gmx.de>
2023-09-03 09:08:08 +0200

    refactor, document dataframe

--- bogan/lib/dataframe.py
+++ bogan/lib/dataframe.py
@@ -8,7 +8,7 @@ log = get_logger(__file__)
 
 
 class Dataframe:
-    def __init__(self, query, cols=[]) -> None:
+    def __init__(self, query, cols=None) -> None:
         self._query = query
         self._cols = cols
         self._type = self.__get_type()
@@ -30,10 +30,9 @@ class Dataframe:
     def __create_df(self) -> pd.DataFrame:
         data = self.nested_dict
         self.__validate_cols_in_dictionary(data)
-        if self._cols:
-            return pd.DataFrame(data, columns=self._cols)
-        else:
-            return pd.DataFrame(data)
+        
+        return pd.DataFrame(data, columns=self._cols)
+        
 
     def __row_dict(self, row: Union[SpielerPos, Partie]) -> dict:
         full_dict = {}
@@ -66,42 +65,46 @@ class Dataframe:
         return full_dict
 
     def __validate_cols_in_dictionary(self, data: List[dict]) -> None:
-        # Warning if dictionary is not valid
+        # Warning if nested dictionary has wrong columns
         # leeres dictionary
         if data:
-            first = data[0]
+            first_row = data[0]
         else:
             log.warning("Das zu validierende dictionary ist leer!")
-            first = {}
+            first_row = {}
             return
 
         # falsche columnss
-        missing_cols = [col for col in self._cols if col not in first.keys()]
-        if missing_cols:
-            log.warning(f"Columns {missing_cols} are missing in the nested_dict. Set all to 'NAN' in column")
+        if self._cols:
+            missing_cols = [col for col in self._cols if col not in first_row.keys()]
+            if missing_cols:
+                log.warning(f"Columns {missing_cols} are missing in the nested_dict. Set all to 'NAN' in column")
 
-    def _default_none(self, value, self_value):
+    def __default_none(self, value, self_value) -> None:
         """Set value to specific self_value if value is None
 
         Args:
             value (any): Value to check
             self_value (any): class variable to set
+
+        Returns:
+            None
         """
         # Default: value = self.value
         if value is None:
             return self_value
 
-    def _needed_cols(self, columns: list) -> None:
+    def __needed_cols(self, columns: list) -> None:
         for col in columns:
             if col not in columns:
                 raise KeyError(f"Not all columns are available, need: {columns}")
 
     def add_position(self, df: pd.DataFrame = None, inplace=True) -> pd.DataFrame:
-        # add 'position' column: position of player in game, depends on punktzahl
-        df = self._default_none(df, self.df)
+        # add 'position' column: Position des Spielers, abhängig von der erreichten Punktzahl
+        df = self.__default_none(df, self.df)
 
         # only works if col "punktzahl" is used
-        self._needed_cols(["punktzahl", "partie_id"])
+        self.__needed_cols(["punktzahl", "partie_id"])
 
         group_df = df.groupby("partie_id")
         df["position"] = group_df["punktzahl"].rank(ascending=False)
@@ -112,9 +115,9 @@ class Dataframe:
         return df
 
     def add_num_players(self, df: pd.DataFrame = None, inplace=True) -> pd.DataFrame:
-        # add 'num_players' column: number of players participate in on game(partie_id)
-        df = self._default_none(df, self.df)
-        self._needed_cols(["partie_id", "spieler"])
+        # add 'num_players' column: Anzahl der Spieler pro Partie (partie_id)
+        df = self.__default_none(df, self.df)
+        self.__needed_cols(["partie_id", "spieler"])
 
         df["num_players"] = df.groupby("partie_id")["spieler"].transform("count")
 
@@ -126,12 +129,16 @@ class Dataframe:
     def add_rankpoints(
         self, df: pd.DataFrame = None, complexity=False, method: str = None, inplace=True
     ) -> pd.DataFrame:
-        # TODO: Has to be completly refactored
         # Add 'rankpoints' column: rankpoints of a game, depends on the position achieved
 
-        df = self._default_none(df, self.df)
-        self._needed_cols(["complexity", "partie_id"])
+        df = self.__default_none(df, self.df)
+        self.__needed_cols(["complexity", "partie_id"])
 
+        # Add num_players if not there
+        if "num_players" not in df.columns:
+            self.add_num_players()
+
+        # TODO: Refactor matching complexity
         match method:
             case "max_point_per_player":
                 if not complexity:
@@ -154,6 +161,19 @@ class Dataframe:
         if inplace:
             self.df = df
         return df
+    
+    def strip_cols_to(self, columns: List[str], df:pd.DataFrame=None, inplace=True) -> pd.DataFrame:
+        # Reduziere DF auf columns
+        
+        df = self.__default_none(df, self.df)
+
+        df = df[columns]
+
+        if inplace:
+            self.df = df
+        return df
+
+
 
 
 class CalcRank:
@@ -205,6 +225,10 @@ if __name__ == "__main__":
     # print(s._query)
     # print(s._type)
     s.add_position()
-    s.add_num_players()
-    s.add_rankpoints(method="max_point_per_player",complexity=True)
+    #s.add_num_players()
+    s.add_rankpoints(method="max_point_per_player",complexity=False)
     print(s.df.head(20))
+    print(s.df.columns)
+
+    s.strip_cols_to(["partie_id", "ort", "spieler"])
+    print(s.df.head(2))
