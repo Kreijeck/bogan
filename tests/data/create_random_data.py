45f21b4b65cb419b01c328b637589b9e1e4c449f
thomas <thomas-strehler@gmx.de>
2023-09-03 23:10:07 +0200

    write random data as string to txt

--- tests/data/create_random_data.py
+++ tests/data/create_random_data.py
@@ -1,11 +1,13 @@
 import random
+import os
+import json
 from datetime import date, timedelta
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from bogan.db.models import Partie, SpielerPos, Benutzer, Brettspiel, Ort, Base
 
 # Erstelle eine SQLite-Testdatenbank im Speicher
-engine = create_engine('sqlite:///tests/data/test_spiel.db')
+engine = create_engine('sqlite:///tests/data/test_spiel2.db')
 
 # Erstelle neue Datenbank
 Base.metadata.drop_all(engine)
@@ -38,12 +40,20 @@ session.commit()
 
 # Erstelle einige Partien mit Spielern
 partien = []
-for _ in range(100):
+data_string = "[\n"
+for counter in range(100):
+    data_string +=f"# Partie {counter + 1}\n"
+    data_string+= "{\n"
     brettspiel = random.choice(brettspiele)
     ort = random.choice(orte_liste)
     datum = date.today() - timedelta(days=random.randint(1, 600))
     spieler = random.sample(benutzer_liste, random.randint(2, len(benutzer_liste)))
     
+    data_string+=f'"brettspiel": brettspiel["{brettspiel.name}"],\n'
+    data_string+=f'"ort": orte_namen["{ort.name}"],\n'
+    data_string+=f'"datum": datetime.strptime("{str(datum)}", "%Y-%m-%d"),\n'
+    data_string+= '"spieler": [\n'
+
     # Erstelle Partie ohne Spieler
     partie = Partie(brettspiel=brettspiel, datum=datum, ort=ort)
     session.add(partie)
@@ -55,13 +65,25 @@ for _ in range(100):
         # Spieler mit größter Punktzahl erhält "win"
         punktzahl = punktzahl_liste[i]
         win = True if punktzahl == max(punktzahl_liste) else False
+        
+        data_string+= f'SpielerPos(punktzahl={punktzahl}, benutzer=benutzer_namen["{benutzer.name}"], win={win}),\n'
         spieler_pos = SpielerPos(punktzahl=punktzahl, win=win, benutzer=benutzer)
         partie.spieler.append(spieler_pos)
+    
+    data_string+= '],\n'
+    data_string+= '},\n'
     
     session.commit()
     partien.append(partie)
 
+data_string+=']'
 session.commit()
 
 # Schließe die Session
 session.close()
+
+# Schreibe List-dictionary in a file:
+cwd = os.path.dirname(__file__)
+filename = os.path.join(cwd, "data_random.txt")
+with open(filename, 'w') as file:
+    file.write(data_string)
