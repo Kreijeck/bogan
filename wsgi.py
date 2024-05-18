ad623fa11ba5e1ceea1b38d51b1e5b2044f68934
thomas <thomas-strehler@gmx.de>
2024-05-18 19:05:13 +0200

    change pythonversion to 3.10

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,6 @@ from bogan import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3181, debug=False)
+    app.run(port=8131,debug=False)
 else:
     application = app
