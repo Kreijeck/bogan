af458e45d2ce0bcd98118dde22604f177e11197e
thomas <thomas-strehler@gmx.de>
2024-05-26 12:07:35 +0200

    Function add_game has been implemented, inkl. html-pages

--- bogan/db/query_ops.py
+++ bogan/db/query_ops.py
@@ -5,12 +5,13 @@ from config import ENCODING
 
 
 # print(bgg.search_boardgame("Wasserkraft"))
+bgg.get_multiple_boardgames()
 
-# json_file = "data/example.json"
+# json_file = "data/example_gaia.json"
 # with open(json_file, 'w', encoding=ENCODING) as f:
-#     json.dump(bgg.get_boardgame_all_stats(251247), f, indent=4, ensure_ascii=False)
+#     json.dump(bgg.get_boardgame_all_stats(396802), f, indent=4, ensure_ascii=False)
 #     print(f"Successfully write Json in {os.path.abspath(json_file)}")
 
-from models import Brettspiel
+# from models import Boardgame
 
-print(Brettspiel().convert_from_bgg_full(bgg.get_boardgame_all_stats(251247)))
+# print(Boardgame().convert_from_bgg_full(bgg.get_boardgame_all_stats(251247)))
