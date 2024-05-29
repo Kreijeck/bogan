c14ac61f1fc1c672e56ee607c819564f10d11140
thomas <thomas-strehler@gmx.de>
2024-05-29 16:25:32 +0200

    p1 - data can be received

--- bogan/db/config.py
+++ bogan/db/config.py
@@ -1,5 +1,7 @@
 # BGG API Information
 ENCODING = "utf-8"
 BGG_BASE_URL = "https://www.boardgamegeek.com/xmlapi2"
+# BGG_BASE_URL = "https://api.geekdo.com/xmlapi2"
+
 # additional xmltodict information
 FORCE_LIST_BBG = ("name", "item")
