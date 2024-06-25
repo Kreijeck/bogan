baf753895951b2c8d405eb8e5f60182440def048
thomas <thomas-strehler@gmx.de>
2024-06-25 21:31:51 +0200

    table for different events with homepage

--- bogan/main/__init__.py
+++ bogan/main/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 
+from bogan.main.lib.event_analysis import create_table, get_game_list
+
 main = Blueprint("main", __name__, template_folder="templates")
 
 
@@ -23,3 +25,9 @@ def search():
 @main.route("/add_game", methods=["POST"])
 def add_game():
     pass
+
+@main.route("/event/<path:event>", methods=["GET"])
+def show_event(event: str):
+    game_list = get_game_list(event)
+    table_data, players = create_table(game_list)
+    return render_template("event.html", game_list=game_list, players=players, table_data=table_data)
