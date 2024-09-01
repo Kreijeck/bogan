74a7231a2d5c2012fea837ef54ea7ebea2fe0339
thomas <thomas-strehler@gmx.de>
2024-09-01 11:40:53 +0200

    fix: API Call Boardgame limited to 20 entries fix: specific tables cleaned up at start

--- bogan/db/update_db.py
+++ bogan/db/update_db.py
@@ -1,4 +1,5 @@
 import json
+from sqlalchemy import MetaData, Table
 from sqlalchemy.orm import Session
 from datetime import datetime
 from bogan.config import ENCODING, GAME_USER
@@ -7,15 +8,32 @@ from bogan.db.ask_bgg import ask_boardgame, ask_games_from
 from bogan.utils import nested_get, get_db_engine
 
 
-engine = get_db_engine(local=False)
-## Delete Database
+engine = get_db_engine(local=True)
+print(f"Datenbank URL {engine.url} wird verwendet")
+session = Session(bind=engine)
+## Delete complete Database
 # Base.metadata.drop_all(engine)
+
+# delete specific tables
+table_to_reset = ["player", "player_pos", "location", "game"]
+metadata = MetaData()
+
+for table_name in table_to_reset:
+    # Versuche die Tabelle mit mit gegebenem Namen zu resetten
+    try:
+        # Erstelle Tabellendefinition
+        table = Table(table_name, metadata, autoload_with=engine)
+        # Lösche die Tabelle
+        table.drop(engine)
+        print(f"Cleared table {table_name}")
+    except Exception as e:
+        print(f"Table {table_name} does not exist or could not be dropped: {e}")
+
 ## Create Database
 Base.metadata.create_all(engine)
-# session = Session(engine)
 
 
-def get_boardgames(session: Session, my_games: dict) -> dict[Boardgame]:
+def get_boardgames(my_games: dict) -> dict[Boardgame]:
     """Update aller Boardgames in der Datenbank.
     Um nur einen API Call durchzuführen werden hier erst alle IDs gesammelt und dann mit einem Call die Stats angefragt
 
@@ -38,7 +56,7 @@ def get_boardgames(session: Session, my_games: dict) -> dict[Boardgame]:
     # Erhalte alle boardgames der angegebenen ids
     boardgames = ask_boardgame(ids) if ids else []
 
-    # Führe ein update bei allen Spielen durch oder lege es neu an
+    # Perform update oder lege ein neues Brettspiel an
     for boardgame in boardgames:
 
         boardgame_db = session.query(Boardgame).filter_by(bgg_id=boardgame.bgg_id).first()
@@ -46,7 +64,7 @@ def get_boardgames(session: Session, my_games: dict) -> dict[Boardgame]:
         # Update mit aktuellen Daten
         if boardgame_db:
             boardgame_db = boardgame_db.update(boardgame)
-            # print(f" Update {boardgame_db.name} with id {boardgame_db.bgg_id}")
+            print(f" Update {boardgame_db.name}, ID: {boardgame_db.bgg_id}")
 
         # Erstelle neuen Eintrag
         else:
@@ -61,77 +79,102 @@ def get_boardgames(session: Session, my_games: dict) -> dict[Boardgame]:
     return boardgames_dict
 
 
-def get_location(session: Session, json_file: dict) -> Location:
+def get_location(json_file: dict) -> Location:
+    """Erstelle location des Spiels falls sie nicht existiert
+
+    Args:
+        json_file (dict): einzelnes Spiel von BGG
+
+    Returns:
+        Location: object von Location
+    """
     name = json_file.get("@location")
 
     location = session.query(Location).filter_by(name=name).first()
 
-    # mit aktueller Parameterliste, kann nur neuer Eintrag erzeugt werden oder nichts passiert
+    # Füge neue location hinzu
     if not location:
         location = Location(name=name)
+        session.add(location)
 
-    session.add(location)
     return location
 
 
-def get_player(session: Session, json_file: dict) -> Player:
+def get_player(json_file: dict) -> Player:
+    """Erstelle Spieler falls er nicht existiert
+
+    Args:
+        json_file (dict): einzelnes Spiel von BGG
+
+    Returns:
+        Player: spieler Object
+    """
     name = json_file.get("@name")
     bgg_name = json_file.get("@username")
     bgg_name = bgg_name if bgg_name else None
 
     player = session.query(Player).filter_by(name=name).first()
 
-    if player:
-        player.bgg_name = bgg_name
-    else:
+    if not player:
         player = Player(name=name, bgg_name=bgg_name)
+        session.add(player)
 
-    session.add(player)
     return player
 
 
-def get_player_pos(session: Session, json_file: dict, game: Game, player: Player) -> PlayerPos:
+def get_player_pos(json_file: dict, game: Game, player: Player) -> PlayerPos:
+    """Erstelle Spieler + Punkte aus einem Spiel
+
+    Args:
+        json_file (dict): eine Zeile spieler pos
+        game (Game): welches spiel gespielt wurde
+        player (Player): welcher spieler verwendet wird
+
+    Returns:
+        PlayerPos: object aus spieler + punkte
+    """
     points = nested_get(json_file, ["@score"], float)
     win = True if json_file.get("@win") == "1" else False
 
     player_pos = session.query(PlayerPos).filter_by(game=game, player=player).first()
 
-    if player_pos:
-        player_pos.points = points
-        player_pos.win = win
-        player_pos.player_id = player.id
-    else:
+    if not player_pos:
         player_pos = PlayerPos(points=points, win=win, game=game, player=player)
-
-    session.add(player_pos)
+        session.add(player_pos)
     return player_pos
 
 
-def get_game(session: Session, json_file: dict, boardgame: Boardgame, location: Location) -> Game:
+def get_game(json_file: dict, boardgame: Boardgame, location: Location) -> Game:
+    """erstelle ein Spiel
+
+    Args:
+        json_file (dict): ein spiel aus BGG
+        boardgame (Boardgame): brettspiel das gespielt wurde
+        location (Location): ort an dem gespielt wurde
+
+    Returns:
+        Game: object des spiels
+    """
     game_bgg_id = nested_get(json_file, ["@id"], int)
     datum = datetime.strptime(json_file.get("@date"), "%Y-%m-%d").date()
     playtime = nested_get(json_file, ["@length"], int)
 
     game = session.query(Game).filter_by(game_bgg_id=game_bgg_id).first()
 
-    if game:
-        game.datum = datum
-        game.playtime = playtime
-        game.boardgame_id = boardgame.id
-        game.location_id = location.id
-    else:
+    if not game:
         game = Game(game_bgg_id=game_bgg_id, datum=datum, playtime=playtime, boardgame=boardgame, location=location)
-        # TODO remove print
-        print(
-            f"neue Partie hinzugefügt: Game(game_bgg_id={game_bgg_id}, brettspiel={game.boardgame.name}, datum={game.datum})"
-        )
-
-    session.add(game)
+        session.add(game)
 
     return game
 
 
 def update_db(from_api: bool, save_file=False):
+    """Update database with new values
+
+    Args:
+        from_api (bool): True: make a real api call, False: used save-file
+        save_file (bool, optional): shall the json be saved. Defaults to False.
+    """
 
     save_path = "data/example/example_plays.json"
 
@@ -150,21 +193,22 @@ def update_db(from_api: bool, save_file=False):
             print("Receive games from local save-file")
             my_games = json.load(file)
 
-    with Session(engine) as session:
-        # First fetch all boardgmes
-        boardgames_dict = get_boardgames(session, my_games)
+    # First fetch all boardgmes
+    boardgames_dict = get_boardgames(my_games)
 
-        # Run database update/creation
-        for my_game in my_games:
-            boardgame_obj = boardgames_dict[nested_get(my_game, ["item", "@objectid"], int)]
-            location_obj = get_location(session, my_game)
-            game_obj = get_game(session, my_game, boardgame_obj, location_obj)
+    # Run database update/creation
+    for my_game in my_games:
+        boardgame_obj = boardgames_dict[nested_get(my_game, ["item", "@objectid"], int)]
+        location_obj = get_location(my_game)
+        game_obj = get_game(my_game, boardgame_obj, location_obj)
 
-            for player_pos_dict in my_game.get("players").get("player"):
-                player_obj = get_player(session, player_pos_dict)
-                get_player_pos(session, player_pos_dict, game_obj, player_obj)
+        for player_pos_dict in my_game.get("players").get("player"):
+            player_obj = get_player(player_pos_dict)
+            get_player_pos(player_pos_dict, game_obj, player_obj)
 
-            print(f"Update game id={game_obj.game_bgg_id} - {game_obj.boardgame.name} on {game_obj.datum}")
+        print(f"Erstelle Partie: game id={game_obj.game_bgg_id} - {game_obj.boardgame.name} on {game_obj.datum}")
+
+        # Speicher Datenbank
         session.commit()
 
 
@@ -172,8 +216,8 @@ if __name__ == "__main__":
     from time import time
 
     t_start = time()
-    update_db(from_api=True)
+    update_db(from_api=False)
     t_stop = time()
-    # TODO remove print
     t_ges = round(t_stop - t_start, 4)
+    # TODO remove print
     print(f"SUCCESS: Das Updaten/Erstellen der Datenbank dauerte {t_ges} sek")
